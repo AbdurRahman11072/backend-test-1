f9afc6017cab9d2e2defb5501efb257600643f2f
AbdurRahman11072 <rjrahman014@gmail.com>
2026-01-17 13:20:18 +0600

    update CORS methods to include PATCH and remove OPTIONS

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(
       "http://localhost:3000",
       "https://green-university-roan.vercel.app",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }),
 );
 app.use(express.json());
